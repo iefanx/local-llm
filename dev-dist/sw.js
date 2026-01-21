13a99db41a455235ee192708edc255a35b3c73b3
iefan <66840141+iefanx@users.noreply.github.com>
2026-01-21 20:51:41 +0530

    feat: Update service worker revision and add asset links for Android app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.tgphsq4f0jg"
+    "revision": "0.rdsi252tl3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
